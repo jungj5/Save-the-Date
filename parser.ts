c43eb34a2c1e3c04f99d7e34611af945e1820480
Jason Woods <j36woods@gmail.com>
2016-10-22 13:44:05 -0400

    Added basic highlighting functionality

--- parser.ts
+++ parser.ts
@@ -1,37 +1,60 @@
 /// <reference path="node_modules/@types/jquery/index.d.ts" />
+const MIN_PARSABLE_TEXT_LENGTH: number = 3;
 
+console.log("I AM PENCIL-IT-IN, FEEL MY WRATH");
 // Interface describing the variables for the MutationObserver
 interface ObserverConfig {
-	attributes: boolean;
-	childList: boolean;
-	characterData: boolean;
+    attributes: boolean;
+    childList: boolean;
+    characterData: boolean;
+    subtree: boolean;
+}
+
+interface MarkOptions {
+    "separateWordSearch": boolean;
 }
 
 // Function to parse all visible text
-function parseDom(): void {
-	let visibleText: string = $(document.body).children(":visible").text();
-	visibleText = visibleText.replace(/\s+/g, " ");
-	console.log(visibleText);
+function parseDom(element: HTMLElement): void {
+    // var context = visibleNodes[i];
+    let parserResults = chrono.parse($(element).text());
+    for (let i: number = 0; i < parserResults.length; i++) {
+        let matchedText: string = parserResults[i].text;
+        console.log(matchedText);
+        let instance: Mark = new Mark(element);
+        let options: MarkOptions = {
+            "separateWordSearch": false
+        }
+        instance.mark(matchedText, options);
+    }
 }
 
-// Function that sets up observers and reparses the 
+// Function that sets up observers and reparses the
 //  page every time there is a change
-$(document).ready(function (): void {
-	let observer: MutationObserver = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation): void {
-			parseDom();
-		});    
-	});
-	 
-	//Notify me of everything!
-	let observerConfig: ObserverConfig = {
-		attributes: true, 
-		childList: true, 
-		characterData: true 
-	};
-	 
-	// Node, config
-	// In this case we'll listen to all changes to body and child nodes
-	let targetNode: HTMLElement = document.body;
-	observer.observe(targetNode, observerConfig);
+$(document).ready(function(): void {
+    parseDom(document.body);
+    let observer: MutationObserver = new MutationObserver(function(mutations) {
+        // Called every time a DOM mutation occurs
+        mutations.forEach(function(mutation): void {
+            var newNodes = mutation.addedNodes;
+            if (newNodes) {
+                $(newNodes).each(function(index, node: HTMLElement) {
+                    parseDom(node);
+                })
+            }
+        });
+    });
+
+    //Notify me of everything!
+    let observerConfig: ObserverConfig = {
+        attributes: false,
+        childList: true,
+        characterData: true,
+        subtree: true
+    };
+
+    // Node, config
+    // In this case we'll listen to all changes to body and child nodes
+    let targetNode: HTMLElement = document.body;
+    observer.observe(targetNode, observerConfig);
 });
