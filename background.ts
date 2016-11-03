923636a5afdd96f522a38301a774a4cba82e40d8
Bikram Bajwa <bikramsbajwa@yahoo.com>
2016-11-03 17:38:38 -0400

    Added code to correct the date/time format of chosen events

--- background.ts
+++ background.ts
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 var events;
+var gapi;
 
 function main() {
   //oauth2 auth
@@ -29,7 +30,7 @@ function main() {
 
   function authorize() {
       gapi.auth.authorize({
-          client_id: '955471480917-2n5jm56c3uucharlj9njl17kbmg72r5h.apps.googleusercontent.com',
+          client_id: '25542497210-94g14jpjos5tmvl0nlg9q3tmuhanu5q6.apps.googleusercontent.com',
           immediate: true,
           scope: 'https://www.googleapis.com/auth/calendar'
       }, function () {
@@ -38,9 +39,6 @@ function main() {
   }
 
   document.getElementById("Create").addEventListener("click", createEvents);
-  
-
-
 }
 
 //function to load upcoming events from the user's calendar
@@ -83,51 +81,91 @@ function createEvents() {
     var Event_Location = $("#location_input").val();
     var Event_Start_Date = $("#start_date").val();
     var Event_End_Date = $("#end_date").val();
+    var Event_Description = $("#textarea1").val();
 
     //Nov 1, 2016
     var Months = {
-      'Jan' : '01',
-      'Feb' : '02',
-      'Mar' : '03',
-      'Apr' : '04',
-      'May' : '05',
-      'Jun' : '06',
-      'Jul' : '07',
-      'Aug' : '08',
-      'Sep' : '09',
-      'Oct' : '10',
-      'Nov' : '11',
-      'Dec' : '12'
-    }
-    //06:30 PM
+        'Jan': '01',
+        'Feb': '02',
+        'Mar': '03',
+        'Apr': '04',
+        'May': '05',
+        'Jun': '06',
+        'Jul': '07',
+        'Aug': '08',
+        'Sep': '09',
+        'Oct': '10',
+        'Nov': '11',
+        'Dec': '12'
+    };
 
-    console.log(Event_Start_Date);
+    //format dates to work with Google Calendar API
+    var Event_Start_Date = Event_Start_Date.slice(-4) + "-" + Months[Event_Start_Date.slice(0, 3)] + "-" + Event_Start_Date.slice(4, 5);
+    var Event_End_Date = Event_End_Date.slice(-4) + "-" + Months[Event_End_Date.slice(0, 3)] + "-" + Event_End_Date.slice(4, 5);
+
+    var start_time = $("#timepicker1").val().slice(0, -3);
+    var end_time = $("#timepicker2").val().slice(0, -3);
+
+    //Case handles 12:xx AM
+    if ($("#timepicker1").val().slice(-2) == "AM") {
+      if (start_time[0] + start_time[1] == "12") {
+
+        start_time = "00" + start_time.slice(2, -3);
+      }
+      else{
+        start_time = start_time.slice(-3);
+      }
+    }
+    if ($("#timepicker2").val().slice(-2) == "AM") {
+      if (end_time[0] + end_time[1] == "12") {
+
+        end_time = "00" + end_time.slice(2, -3);
+      }
+      else{
+        end_time = end_time.slice(-3);
+      }
+    }
+
+    //Case handles converting PM times
+    if ($("#timepicker1").val().slice(-2) == "PM") {
+      if (start_time[0] + start_time[1] != "12") {
+        var hour = start_time[0] + start_time[1]
+        hour = parseInt(hour) + 12;
+        hour = hour.toString();
+        start_time = hour + start_time.slice(2);
+      }
+    }
+    if ($("#timepicker2").val().slice(-2) == "PM") {
+      if (end_time[0] + end_time[1] != "12") {
+        var hour = end_time[0] + end_time[1]
+        hour = parseInt(hour) + 12;
+        hour = hour.toString();
+        end_time = hour + end_time.slice(2);
+      }
+    }
 
     var event = {
         'summary': Event_Summary,
         'location': Event_Location,
-        'description': 'Testing event creating for P-I-I app',
+        'description': Event_Description,
         'start': {
-            'dateTime': Event_Start_Date + 'T09:00:00',
+            'dateTime': Event_Start_Date + 'T' + start_time + ':00',
             'timeZone': 'America/New_York'
         },
         'end': {
-            'dateTime': Event_End_Date + "T09:00:00",
-            'timeZone': 'America/Los_Angeles'
-        },
+            'dateTime': Event_End_Date + 'T' + end_time + ':00',
+            'timeZone': 'America/New_York'
+        }
     };
-    var request = gapi.client.calendar.events.insert(
-        {
+    var request = gapi.client.calendar.events.insert({
         'calendarId': 'primary',
         'resource': event
-        }
-    );
-    request.execute(function (event) 
-        {
-          display('Event created: ' + event.htmlLink);
-        }
-    );
+    });
+    request.execute(function (event) {
+        display('Event created: ' + event.htmlLink);
+    });
 }
+
     
 //function to add a message to the popup
 function display(message) {
